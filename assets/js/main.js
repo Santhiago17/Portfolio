7035e062385b24a78001a6bf4b8e7b27f87853b2
Santhiago <santhiagosantos7@gmail.com>
2023-03-13 11:02:23 -0300

    Preenchendo dados do perfil

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,7 +1,25 @@
 
+function updateProfileInfo(profileData){
+    document.getElementById('profile.photo')
+    photo.src = profileData.photo
+    photo.alt = profileData.name
+
+    const name = document.getElementById('profile.name')
+    name.innerText = profileData.name
+
+    const job = document.getElementById('profile.job')
+    job.innerText = profileData.job
+
+    const location = document.getElementById('profile.location')
+    location.innerText = profileData.location
+
+    const contact = document.getElementById('profile.contact')
+    contact.innerText = profileData.contact
+}
+
 
 (async  () => {
     const profileData = await fetchProfileData()
-    console.log(profileData)
+    updateProfileInfo(profileData)
 })()
 
